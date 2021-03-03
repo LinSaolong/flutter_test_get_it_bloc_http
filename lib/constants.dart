90c5ce10ae18c5108d86b53ab495604eb59c31f9
Saolong Lin <60967691+LinSaolong@users.noreply.github.com>
2021-03-03 07:46:00 +0700

    shadow

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+LinearGradient kLinear = LinearGradient(
+  begin: Alignment(-.8, -0.8),
+  end: Alignment(1.8, -0.5),
+  stops: [0.12, 0.93, 0.93],
+  colors: [
+    Color(0xFF242424),
+    Color(0xFF484848),
+    Color(0xFF484848),
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test_get_it_bloc_http/constants.dart';
 import 'package:flutter_test_get_it_bloc_http/injection_container.dart';
 
 import 'features/presentation/bloc/province/province_bloc.dart';
@@ -36,38 +39,105 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: BlocProvider(
-          create: (context) => ProvinceBloc(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<ProvinceBloc, ProvinceState>(
-                builder: (context, state) {
-                  if (state is ProvinceLoading) {
-                    return CircularProgressIndicator();
-                  } else if (state is ProvinceLoaded) {
-                    return Column(
-                      children: [
-                        ...List.generate(state.listProvince.length,
-                            (index) => Text(state.listProvince[index].text))
-                      ],
-                    );
-                  } else {
-                    return Text(
-                      '_counter',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  }
-                },
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(-.8, -0.8),
+            end: Alignment(1.8, -0.5),
+            stops: [0.12, 0.93, 0.93],
+            colors: [
+              Color(0xFF242424),
+              Color(0xFF484848),
+              Color(0xFF484848),
             ],
+          ),
+        ),
+        child: Center(
+          child: BlocProvider(
+            create: (context) => ProvinceBloc(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    // gradient: RadialGradient(
+                    //   radius: .8,
+                    //   colors: [
+                    //     Color(0xFF484848),
+                    //     Color(0xFF242424).withOpacity(.5),
+                    //   ],
+                    // ),
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(-1, -1),
+                        // spreadRadius: 1,
+                        // blurRadius: 2,
+                      ),
+                      BoxShadow(
+                        offset: Offset(1, 1),
+                        color: Colors.white.withOpacity(.2),
+                      ),
+                    ],
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      // gradient: RadialGradient(
+                      //   radius: 1.1,
+                      //   colors: [
+                      //     Color(0xFF484848),
+                      //     Color(0xFF242424),
+                      //   ],
+                      // ),
+                      borderRadius: BorderRadius.circular(50),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(-1, -1),
+                          // spreadRadius: 1,
+                          // blurRadius: 2,
+                        ),
+                        BoxShadow(
+                          offset: Offset(1, 1),
+                          color: Colors.white.withOpacity(.2),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      'Button',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                BlocBuilder<ProvinceBloc, ProvinceState>(
+                  builder: (context, state) {
+                    if (state is ProvinceLoading) {
+                      return CircularProgressIndicator();
+                    } else if (state is ProvinceLoaded) {
+                      return Column(
+                        children: [
+                          ...List.generate(state.listProvince.length,
+                              (index) => Text(state.listProvince[index].text))
+                        ],
+                      );
+                    } else {
+                      return Text(
+                        '_counter',
+                        style: Theme.of(context).textTheme.headline4,
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
